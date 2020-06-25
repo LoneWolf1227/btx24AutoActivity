b8d24c27afcc214fbbb258ca690ec2b8392f0e8a
LoneWolf1227 <48411716+LoneWolf1227@users.noreply.github.com>
2020-06-25 20:37:03 +0500

    Update BTX24.php

--- libs/BTX24.php
+++ libs/BTX24.php
@@ -3,16 +3,15 @@
 class BTX24{
 
     function method($method, $params){
-        $response = '';
-        //if (array_key_exists('saved', $_REQUEST)) {
-            //$this->defaults = $_REQUEST;
-            $c = curl_init('https://your_bitrix24_subdomain.bitrix24.ru/rest/1/you_api_key/' . $method);
-            curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
-            curl_setopt($c, CURLOPT_POST, true);
-            curl_setopt($c, CURLOPT_POSTFIELDS, http_build_query($params));
-            $response = curl_exec($c);
-            $response = json_decode($response, true);
-        //}
+        
+        $url = 'https://your_bitrix24_subdomain.bitrix24.ru/rest/1/you_api_key/' . $method;
+        
+        $c = curl_init($url);
+        curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($c, CURLOPT_POST, true);
+        curl_setopt($c, CURLOPT_POSTFIELDS, http_build_query($params));
+        $response = curl_exec($c);
+        $response = json_decode($response, true);
 
         return $response;
     }
