d0f74f944117c860b38b17fa5d478213e31da9b6
LoneWolf1227 <48411716+LoneWolf1227@users.noreply.github.com>
2020-06-25 20:26:37 +0500

    Update BTX24.php

--- libs/BTX24.php
+++ libs/BTX24.php
@@ -2,13 +2,11 @@
 
 class BTX24{
 
-    //public $defaults = array('first_name' => '', 'phone' => '', 'dolgnost' => '', 'email' => '');
-
     function method($method, $params){
         $response = '';
         //if (array_key_exists('saved', $_REQUEST)) {
             //$this->defaults = $_REQUEST;
-            $c = curl_init('https://b24-ymflsh.bitrix24.ru/rest/1/vyf8418tywover3t/' . $method);
+            $c = curl_init('https://b24-ymflsh.bitrix24.ru/rest/1/you_api_key/' . $method);
             curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
             curl_setopt($c, CURLOPT_POST, true);
             curl_setopt($c, CURLOPT_POSTFIELDS, http_build_query($params));
